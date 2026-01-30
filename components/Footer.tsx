917ac197ecdc20c9849ee00115d54ace4a8124f5
Tom Andrews <161226554+TomAndrewsRecGroup@users.noreply.github.com>
2026-01-30 12:11:55 +0000

    Create Footer.tsx

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="w-full py-8 border-t border-white/10 bg-black text-center text-xs text-gray-500">
+      © 2024 RecXchange. All rights reserved.
+    </footer>
+  );
+}
